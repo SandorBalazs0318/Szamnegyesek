4d4330af93aa8fb9c042ba7bdf6dd65eac767526
c9sanbal <c9sanbal@SZT3-7-W10>
2025-03-07 10:30:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Szamnegyesek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] result = TableGame([[1, 2, 1], [2, 4, 2], [1, 2, 1]]);
+            string kiiras = string.Join(", ", result);
+            Console.WriteLine("[" + kiiras + "]");
+        }
+
+        static int[] TableGame(int[][] tabla)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (tabla[0][0] + tabla[0][2] != tabla[0][1])
+                    {
+                        return [-1];
+                    }
+                    if (tabla[0][0] + tabla[2][0] != tabla[1][0])
+                    {
+                        return [-1];
+                    }
+                    if (tabla[2][0] + tabla[2][2] != tabla[2][1])
+                    {
+                        return [-1];
+                    }
+                    if (tabla[0][2] + tabla[2][2] != tabla[1][2])
+                    {
+                        return [-1];
+                    }
+                    if (tabla[0][0] + tabla[0][2] + tabla[2][0] + tabla[2][2] != tabla[1][1])
+                    {
+                        return [-1];
+                    }
+                }
+            }
+            return new int[] { tabla[0][0], tabla[0][2], tabla[2][0], tabla[2][2] };
+        }
+    }
+}
